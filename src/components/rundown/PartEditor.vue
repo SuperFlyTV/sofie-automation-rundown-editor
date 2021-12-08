71647c57d95a8d92d1b69a33c144c31d5f2bd74c
Alexander Lange Van Camp <vancamp.alex@gmail.com>
2021-12-08 06:02:22 -0600

    fix: fix error when deleting a Part (#11)
    Closes #8

--- src/components/rundown/PartEditor.vue
+++ src/components/rundown/PartEditor.vue
@@ -84,7 +84,7 @@ export default Vue.extend({
 		},
 		rundown: {
 			get(): Partial<Rundown> {
-				return { ...(store.state.rundowns.find((r) => r.id === this.id) || {}) }
+				return { ...(store.state.rundowns.find((r) => r.id === this.$route.params.id) || {}) }
 			}
 		},
 		part() {
